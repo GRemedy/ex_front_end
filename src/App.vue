f07dd7837362b2582e04ef7ce4e79145d0dfad2f
Gremedy <remdedy@remedy.com>
2023-05-30 21:35:15 +0800

    基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import login from './pages/login.vue';
+import home from './pages/home.vue';
 </script>
 
 <template>
-
+<login></login>
+<home></home>
 </template>
 
 <style scoped>
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-container>
+      <myHeader></myHeader>
+      <el-container>
+        <myAside></myAside>
+        <myMain></myMain>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:"home"
+    }
+    import myAside from '../components/myAside.vue';
+    import myMain from '../components/myMain.vue';
+    import myHeader from '../components/myHeader.vue';
+</script>
+
+<style lang="css" scoped>
+
+</style>
